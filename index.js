7c7a08ad98a7b50c0cace5d42a5f7072fea1fd61
Zhixuan Zhang <chi@Zhixuans-MacBook-Pro.local>
2019-11-17 04:26:17 -0500

    update

---
+++ index.js
@@ -0,0 +1,56 @@
+console.log("please check this file.");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// If you happen to be reading this, can you please do something to Colin. I know she’s at least not uncomfortable with you. She’s visiting Rochester, which is why I got the chance of sneaking this message in.
+
+// She's not in her best state. Worst, more likely. She can't sleep well, or even just sleep. She's been having these repeating nightmares of her trauma for six years nightly. Her PTSD is bad, so is her MDD and anxiety disorder and bipolar disorder.
+// She is living with two cats, she can't take care of herself, nor can the cats take care of her. She only remembers to take good care of the cats. She goes without eating proper meal for days and cuts her wrist instead. Walking outside and going to classes terrifies her more than how most of us can imagine. Her flashbacks and nightmares are daily and it’s basically making every day of her life the worst day repeating. I can’t help at all to be honest.
+
+// She's pulling through somehow. She's been finishing classes with usually good performance. She's trying to reach her goals of being like a normal person, very desperately that it is almost not my place to stop her anymore. I don’t have the ability either.
+// I have to admit it is quite possible for her to finish school the way she wants. She calls it the “great gamble”. Means she either finish school or she might be dead by her own hands before that.
+
+// She's been worse than usual the recent month. She's on the verge of going to ER everyday, but scared off by her past experiences of being there and the psych ward. All her treatment providers strongly recommend her at the very least have a partial hospitalization. However she told me that hospitalizations have only made her worse in all her attempts, and I’ve actually seen what those situations do to her. It’s the worst thing for her. They can only secure her physical safety momentarily at the cost of many other things. She couldn’t afford it financially either.
+// She's been a threat to herself, although she's still holding herself back. But I am sick worried and out of reach for helping her right now.
+
+// She has been avoiding making friends or even talking to people for years because she thinks she might scare other people. She treasures all the classes she's taking, she likes all her professors, which is also why I end up sneaking this message to you. You are one of the maybe five people she knows.
+
+// You know, she wants to finish Death Stranding, a game that recently came out that she just started. Code vein, ffxiv, even just Stardew Vally or Minecraft. Any game you can name. She's super excited for the new Pokemon game that just came out, been waiting for months. There's things she still love, her games, her cats, her freedom to draw. I don't feel justified for urging her to go to ER and separating her from those. She's got her complex reasons that I cannot explain either.
+
+// But the things she like only keep her happy for a pitiful amount of time in her daily life. I’m not experienced enough to decide whatever I should be doing to her/ for her. I know it is no one’s obligation to do anything, so I’m not asking you to do anything. You can just forget about this. I’m just trying everything now. But if you would, please try talking to her before everything else. She is a really good liar, you can’t get her telling you how she really feels unless you keep pushing her and peeling off her lies. I just want to relay the information of at least how she really is right now and see what that might do. If anything, please don’t let her know this message existed and not raise it to the school dean level. It would make her very unstable. I’m deleting the local files right after I upload this. I’m sorry for the unwelcomed message you have to take in if you’re still reading this.
